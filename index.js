9862caddfe1c0d961ccfa1550f29d188a86759be
deepu-arepalli <deepikaarepalli217@gmail.com>
2024-06-21 18:06:46 +0530

    update 2

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const vendorRoutes =  require('./routes/vendorRoutes');
 const bodyParser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require("./routes/productRoutes");
+const cors = require('cors');
 
 const path = require('path');
+
 
     const app = express();
     const PORT = process.env.PORT || 4000;
     dotEnv.config();
+    app.use(cors());
 
     mongoose.connect(process.env.MONGO_URI)
         .then(()=> console.log("MongoDB Connected Successfully!"))
