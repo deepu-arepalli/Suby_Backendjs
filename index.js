a95012df0ceec002498456c75c6c67bd46addc28
Deepu-arepalli <147024878+deepu-arepalli@users.noreply.github.com>
2024-06-21 16:00:52 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const vendorRoutes =  require('./routes/vendorRoutes');
 const bodyParser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require("./routes/productRoutes");
-const cors = require('cors');
 const path = require('path');
 
     const app = express();
